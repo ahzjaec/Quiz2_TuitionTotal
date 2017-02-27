4f0a4c6df171dd34ec6d0de5f5fbf0f525b7c8be
Chainie Yates <Chainie Yates@128.4.78.12>
2017-02-27 00:54:30 -0500

    second initial commit

--- TuitionCalculator/src/Calculator.java
+++ TuitionCalculator/src/Calculator.java
@@ -5,24 +5,22 @@ public class Calculator {
 	
 		public static void main(String[] args) {
 			
-				java.util.Scanner input = new Scanner(System.in);
-				
-				System.out.println("How much is your tuition your freshman year?");
-				double tuition = input.nextDouble();
-				
-				System.out.println("What is the percentage increase on your tuition per year?");;
-				double rate = 1 + ((input.nextDouble()) / 100);
-				
-				double Total = 0;
-				for (int i = 0; i < 4; i++){
-					
-					double annualT = tuition * Math.pow(rate,  i);
-					tuition = annualT;
-					Total += annualT;
-					input.close();
-				}
-				
-				System.out.printf("the total tuition to complete your degree is $%4.2f\n" , Total);
+			System.out.println("What is the initial tuition?");
+			Scanner tuition = new Scanner(System.in);
+			float Tuition = tuition.nextFloat();
+			
+			System.out.println("What is the annual percentage increase?");
+			Scanner rate = new Scanner(System.in);
+			float Rate = rate.nextFloat();
+			Rate = Rate/100;
+			
+			float Total = Tuition;
+			
+			for(int year=1; year<4; year++){
+				float annualT = Tuition * (1+Rate);
+				Total = Total + annualT;
+				Tuition = annualT;
+			}
+	System.out.println(String.format("The cost to complete your degree is: %.2f",Total));
 		}
-
 }
